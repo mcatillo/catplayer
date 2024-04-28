d8ae8dd9f81c5d10cd8cb8bd4e573670ee81e963
Marco Catillo <107922582+mcatillo@users.noreply.github.com>
2024-04-28 23:28:55 +0200

    Update header.py

--- src/header.py
+++ src/header.py
@@ -10,6 +10,8 @@ Copyright (C) 2024 Marco Catillo
 Distribuited under GPLv3 license
 https://www.gnu.org/licenses/gpl-3.0.html
 
+Description:
+    class Header implemented
 
 '''
 
@@ -19,7 +21,21 @@ from PySide6.QtCore import *
 from src.language import *
 
 class Header(QLabel):
+    '''Class for path video on the top of the app
+
+    Attributes:
+        arg (str): file video/music to open
+        lang (Language): variable of type class Language in src/language.py 
+            containing the dictionary
+    '''
     def __init__(self,arg,lang):
+        '''Class initialization
+        
+        Parameters:
+            arg (str): file video/music to open
+            lang (Language): variable of type class Language in src/language.py 
+                containing the dictionary
+        '''
         super().__init__()
         self.lang = lang
         self.setHeaderTitle(arg)
@@ -29,6 +45,11 @@ class Header(QLabel):
         self.setCursor(QCursor(Qt.IBeamCursor))
 
     def setHeaderTitle(self,arg):
+        '''Set title of the header
+
+        Parameters:
+            arg (str): file video/music to open
+        '''
         self.arg = arg if arg else self.lang.fromKey('nofileselected')
         self.setText(self.arg)
         self.setToolTip(self.arg)
